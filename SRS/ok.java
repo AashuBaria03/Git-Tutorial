f9565b01079d36925987f361055ace751758d2ca
AashuBaria03 <ashubaria2004@gmail.com>
2025-01-23 07:54:52 +0530

    Version 1

---
+++ SRS/ok.java
@@ -0,0 +1,5 @@
+package SRS;
+
+public class ok {
+    
+}
---
+++ star.java
@@ -0,0 +1,6 @@
+public class star{
+    public static void main(String[] args) {
+        System.out.println("Hello Aashu");
+        System.out.println("Version1");
+    }
+}
